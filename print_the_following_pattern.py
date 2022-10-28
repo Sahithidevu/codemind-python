c782f1371ae2d4f3212ec2e980cba081ab7c6d0a
Devu Divya Sahithi <111502082+Sahithidevu@users.noreply.github.com>
2022-10-28 10:58:03 +0530

    rows=int(input()) for i in range(rows,0,-1):     for j in range(rows,0,-1):         if i==j:             print('0',end='')         else:             print('x',end='')     print()

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,5 +1,8 @@
 rows=int(input())
 for i in range(rows,0,-1):
     for j in range(rows,0,-1):
-        print(j,end=' ')
+        if i==j:
+            print('0',end='')
+        else:
+            print('x',end='')
     print()
